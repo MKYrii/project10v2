09d41e0889ba93da2a97c7a3c7b0bfd207031f4f
MKYrii <90323298+MKYrii@users.noreply.github.com>
2022-05-04 00:49:25 +0300

    Add files via upload

---
+++ project/cmake-build-debug/CMakeFiles/GameStars.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Uriy/CLionProjects/GameStars/Bullet.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/Bullet.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/Character.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/Character.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/EndlessGame.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/EndlessGame.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/ExplosionImage.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/ExplosionImage.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/FastGame.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/FastGame.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/GameScene.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/GameScene.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/ShootingEnemy.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/ShootingEnemy.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/Star.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/Star.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/TargetEnemy.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/TargetEnemy.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/WalkingEnemy.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/WalkingEnemy.cpp.obj"
+  "C:/Users/Uriy/CLionProjects/GameStars/main.cpp" "C:/Users/Uriy/CLionProjects/GameStars/cmake-build-debug/CMakeFiles/GameStars.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/SFML/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/cmake-build-debug/CMakeFiles/GameStars.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameStars.dir/Bullet.cpp.obj"
+  "CMakeFiles/GameStars.dir/Character.cpp.obj"
+  "CMakeFiles/GameStars.dir/EndlessGame.cpp.obj"
+  "CMakeFiles/GameStars.dir/ExplosionImage.cpp.obj"
+  "CMakeFiles/GameStars.dir/FastGame.cpp.obj"
+  "CMakeFiles/GameStars.dir/GameScene.cpp.obj"
+  "CMakeFiles/GameStars.dir/ShootingEnemy.cpp.obj"
+  "CMakeFiles/GameStars.dir/Star.cpp.obj"
+  "CMakeFiles/GameStars.dir/TargetEnemy.cpp.obj"
+  "CMakeFiles/GameStars.dir/WalkingEnemy.cpp.obj"
+  "CMakeFiles/GameStars.dir/main.cpp.obj"
+  "GameStars.exe"
+  "GameStars.exe.manifest"
+  "GameStars.pdb"
+  "libGameStars.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameStars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
